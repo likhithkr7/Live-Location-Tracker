cdc2907649f73e7b9ffd3c8032b20d1d793ac5f0
Likhith.R.Krishna <likhithkr72gmail.com>
2021-05-19 18:00:47 +0530

    Enter

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:trackit/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 
+// Live Location Tracker
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
